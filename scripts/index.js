3fde7c173ff852a2d63900b102545969f41634b3
40588129-napier <40588129@live.napier.ac.uk>
2024-03-23 17:12:05 +0000

    feat: completed landing page

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+//scrolls to featured article section
+function scrollToFeatured() {
+    let element = document.getElementById("featured_articles");
+    element.scrollIntoView();
+}
---
+++ scripts/translate_init.js
@@ -0,0 +1,4 @@
+function googleTranslateElementInit() {
+    //initialises provided google translate element
+    new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+}
