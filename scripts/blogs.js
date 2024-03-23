f7c3b428d6045fb1d26b8ef45c018a46de8cd8a6
40588129-napier <40588129@live.napier.ac.uk>
2024-03-23 22:58:32 +0000

    feat: test url checking

---
+++ scripts/blogs.js
@@ -0,0 +1,27 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    let form = document.getElementById("article_search");
+
+    function submitForm(e){
+        e.preventDefault();
+        let form = new FormData(e.target);
+        let search = form.get("search");
+        let url = window.location.href;
+        let split = url.split("/");
+        split.pop();
+        url = split.join('/') + "/posts/" + search + ".html";
+        urlExists(url)
+        //window.location.assign(url);
+    }
+
+    form.addEventListener('submit', submitForm);
+})
+
+function urlExists(url){
+    const client = new XMLHttpRequest();
+    client.open("GET", url);
+    client.send();
+
+    client.onreadystatechange = (e) => {
+        console.log(client.status)
+    }
+}
