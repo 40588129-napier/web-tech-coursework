b3e2341a152588a61f1b3c982a93d69a0139bb21
40588129-napier <40588129@live.napier.ac.uk>
2024-03-24 13:41:07 +0000

    feat: change page if exists

--- scripts/blogs.js
+++ scripts/blogs.js
@@ -1,22 +1,24 @@
 window.addEventListener("DOMContentLoaded", (event) => {
     let form = document.getElementById("article_search");
-
-    function submitForm(e){
-        e.preventDefault();
-        let form = new FormData(e.target);
-        let search = form.get("search");
-        let url = window.location.href;
-        let split = url.split("/");
-        split.pop();
-        url = split.join('/') + "/posts/" + search + ".html";
-        urlExists(url).then((result) => {
-            console.log(result)
-        })
-        //window.location.assign(url);
-    }
-
     form.addEventListener('submit', submitForm);
 })
+
+function submitForm(e){
+    e.preventDefault();
+    let form = new FormData(e.target);
+    let search = form.get("search");
+    let url = window.location.href;
+    let split = url.split("/");
+    split.pop();
+    url = split.join('/') + "/posts/" + search + ".html";
+    let exists = false;
+    urlExists(url).then((result) => {
+        exists = result;
+    })
+    if(exists){
+        window.location.assign(url);
+    }   
+}
 
 function urlExists(url){
     const client = new XMLHttpRequest();
@@ -25,7 +27,6 @@ function urlExists(url){
         client.send();
         client.onreadystatechange = (e) => {
             if(client.readyState === 4){
-                console.log(client.status == 200)
                 if(client.status == 200){
                     resolve(true);
                 }
