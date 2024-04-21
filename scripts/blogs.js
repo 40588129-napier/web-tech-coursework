5d82d05ae49275a02bf935d1573c05a646704e9c
40588129-napier <40588129@live.napier.ac.uk>
2024-04-21 12:53:58 +0100

    latest site

--- scripts/blogs.js
+++ scripts/blogs.js
@@ -1,21 +1,46 @@
+const existingArticles = [
+    "Chichen Itza",
+    "Colosseum",
+    "Great Wall of China",
+    "Machu Picchu",
+    "Petra",
+    "Taj Mahal"
+]
+
 window.addEventListener("DOMContentLoaded", (event) => {
-    let form = document.getElementById("article_search");
+    let form = document.getElementById("search_form");
+    let input = document.getElementById("search_input");
     form.addEventListener('submit', submitForm);
+    input.addEventListener('keyup', changeSearch);
 })
 
-function submitForm(e){
+function changeSearch(e) {
+    e.preventDefault();
+    populateRecommendations(e.target.value);
+}
+
+function submitForm(e) {
     e.preventDefault();
     let form = new FormData(e.target);
-    let search = form.get("search");
-    let url = window.location.href;
-    let split = url.split("/");
-    split.pop();
-    url = split.join('/') + "/posts/" + search + ".html";
-    if(url.includes("https://")){
-        let exists = false;
+    let search = form.get("search"); //get value entered in input
+    pageSearch(search);
+}
+
+function clickRecommended(list) {
+    pageSearch(list.innerHTML);
+}
+
+function pageSearch(search) {
+    search = search.toLowerCase(); //set search value to lowercase
+    search = search.replaceAll(" ","_"); //replace spaces with underline
+    let url = window.location.href; //get current url
+    let split = url.split("/"); //split url
+    split.pop(); //remove last index, i.e. the file name
+    url = split.join('/') + "/posts/" + search + ".html"; //rejoin url and add new file name
+    if(url.includes("https://")){ //allow local testing by only checking url if https
         urlExists(url).then((exists) => {
             console.log(exists)
-            if(exists){
+            if(exists) { //redirect user if url exists
                 window.location.assign(url);
             }   
         })
@@ -25,7 +50,26 @@ function submitForm(e){
     }
 }
 
-function urlExists(url){
+function populateRecommendations(search) {
+    let recommendationsBox = document.getElementById("recommended_search");
+    let content = [];
+    let recommendationsHTML = "";
+    if(search !== ""){
+        let result = existingArticles.filter((value) => {
+            value = value.toLowerCase();
+            search = search.toLowerCase();
+            search = search.replace(/^the\s/, "");
+            return value.includes(search);
+        });
+        for(let i = 0; i < result.length; i++) {
+            content.push("<li onclick=clickRecommended(this)>" + result[i] + "</li>");
+        }
+        recommendationsHTML =  "<ul>" + content.join("") + "</ul>";
+    }
+    recommendationsBox.innerHTML = recommendationsHTML;
+}
+
+function urlExists(url) {
     const client = new XMLHttpRequest();
     return new Promise((resolve, reject) => {
         client.open("GET", url);
